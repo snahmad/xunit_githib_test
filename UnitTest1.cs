1abc417b288bfd26b1ff52bc4fe8d5532e100cc0
Naeem Ahmad <naeem.ahmad@actisense.com>
2019-06-03 17:25:33 +0100

    ttt

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -9,11 +9,11 @@ namespace xunit_githib_test
         public void Test1()
         {
             int result = 5;
-            Assert.Equal(result, 5);
+            Assert.Equal(5, result);
             //mlll
             //kdkdk
             result = 6;
-            Assert.Equal(result, 6);
+            Assert.Equal(6, result);
         }
 
     }
